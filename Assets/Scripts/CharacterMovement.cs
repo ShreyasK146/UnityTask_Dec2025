2acea4a41067a3d608d478ca305db67c5c267ec2
ShreyasK146 <shreyaskadoor47@gmail.com>
2025-12-04 18:13:44 +0530

    Initial Movement Setup and Project Upload

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private bool isMoving = false;
+    private bool isGrounded = false;
+    [SerializeField] private float runSpeed = 5;
+    [SerializeField] private float jumpHeight = 5;
+    [SerializeField] private float mouseSens = 250f;
+    private CharacterController characterController;
+    private float yRotation;
+    Animator animator;
+    private Vector3 velocity;
+    private Vector3 gravityDir = Vector3.down;
+    private float gravity = 9.81f;
+    Vector3 nextGravityDir = Vector3.down;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }   
+
+    private void Update()
+    {
+        HandleCharacterMovement();
+        HandleMouseMovement();
+        HanldeGravity();
+        HandleJump();
+        UpdateAnimator();
+        ChangeGravityDirection();
+    }
+
+   
+
+    private void HandleCharacterMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
+        characterController.Move(move * runSpeed * Time.deltaTime);
+
+        isMoving = move.magnitude > 0.1f;
+    }
+
+    private void HandleMouseMovement()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+
+        yRotation += mouseX;
+
+        transform.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+    private void HanldeGravity()
+    {
+    
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -gravityDir, out hit, 2f))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+  
+        velocity += gravityDir * gravity * Time.deltaTime;
+
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    private void HandleJump()
+    {
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            velocity = -gravityDir * jumpHeight;
+        }
+    }
+
+
+    private void UpdateAnimator()
+    {
+        if (isGrounded && isMoving)
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else 
+        {
+            Debug.Log(isGrounded);
+            animator.SetBool("isRunning", false);
+        }
+    }
+
+    private void ChangeGravityDirection()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            nextGravityDir = Vector3.left;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            nextGravityDir = Vector3.right;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            nextGravityDir = Vector3.up;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            nextGravityDir = Vector3.down;
+        }
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            gravityDir = nextGravityDir.normalized;
+        }
+        
+    }
+
+}
